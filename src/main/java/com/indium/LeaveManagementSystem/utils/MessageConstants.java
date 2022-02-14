5b24550b833f1d2a6fbe6599a1e81951c0f25fa1
Sajith Xavier <sajithvana@gmail.com>
2022-02-14 19:29:24 +0530

    MessageConstants Updated

--- src/main/java/com/indium/LeaveManagementSystem/utils/MessageConstants.java
+++ src/main/java/com/indium/LeaveManagementSystem/utils/MessageConstants.java
@@ -2,8 +2,25 @@ package com.indium.LeaveManagementSystem.utils;
 
 public class MessageConstants {
 
+    public static final String ACTIVE = "Active";
     public static final String DELETED = "Deleted";
-    public static final String Approved = "Approved";
+    public static final String APPROVED = "Approved";
+    public static final String REJECTED = "Rejected";
+    public static final String PENDING = "Pending";
+    public static final String EMPLOYEE_ADDED ="Employee Detail Added Successfully";
+    public static final String EMPLOYEE_UPDATED ="Employee Detail Updated Successfully";
+    public static final String EMPLOYEE_DELETED ="Employee Detail Deleted Successfully";
+    public static final String BAD_REQUEST_ADD ="Error while adding details";
+    public static final String BAD_REQUEST_GET ="Error while getting details";
+    public static final String BAD_REQUEST_UPDATE ="Error while updating details";
+    public static final String BAD_REQUEST_DELETE ="Error while deleting details";
+    public static final String EMPLOYEE_BY_ID ="Employee Details By Id";
+    public static final String LEAVE_APPROVED ="Leave Approved";
+    public static final String LEAVE_REJECTED ="Leave Rejeted";
+    public static final String INVALID_REQUEST ="Invalid Request";
+    public static final String No_LEAVE_BALANCE ="No Leave Balance";
+
+    //public static final String SUCCESSFULLY_ADDED ="Employee Detail added Successfully";
 
 
 }
